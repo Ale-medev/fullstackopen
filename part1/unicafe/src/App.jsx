5f1943dc9ce471028ab9ccde62f92285611e33ab
Alejandro Medail <amedail.design@gmail.com>
2024-07-03 21:45:56 -0300

    Part 1 exercises 1.7: Unicafe, step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -13,20 +13,37 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  const [sum, setSum] = useState(0)
+
+  const handleClick = (state, setState, score) => {
+    setState(state + 1)
+
+    const updatedTotal = total + 1
+    setTotal(updatedTotal)
+
+    const updateSum = sum + score
+    setSum(updateSum)
+  }
+
+  const average = sum / total
 
   return (
     <div>
       <Headline title='give feedback' />
 
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={() => handleClick(good, setGood, 1)} text="good" />
+      <Button handleClick={() => handleClick(neutral, setNeutral, 0)} text="neutral" />
+      <Button handleClick={() => handleClick(bad, setBad, -1)} text="bad" />
 
       <Headline title='statistics' />
 
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {total === 0 ? 0 : average}</p>
+      <p>positive {total === 0 ? 0 : good * 100 / total}%</p>
     </div>
   )
 }
