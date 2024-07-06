87d84e22c3523878e07e23ae790b0e53c21b158d
Alejandro Medail <amedail.design@gmail.com>
2024-07-06 20:47:01 -0300

    Part 2 exercise 2.1: Course Information, step 6 (corrected)

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,23 +1,17 @@
 const Header = ({ course }) => <h1>{course}</h1>
 
 const Part = ({ part }) => (
-  <p>
+  <li>
     {part.name} {part.exercises}
-  </p>
+  </li>
 )
 
 const Content = ({ parts }) => (
-  <>
-    <Part
-      part={parts[0]} 
-    />
-    <Part
-      part={parts[1]} 
-    />
-    <Part
-      part={parts[2]} 
-    />      
-  </>
+  <ul>
+    {
+      parts.map(part => (<Part key={part.id} part={part} />))
+    }   
+  </ul>
 ) 
 
 const Course= ({ course }) => (
