e1c5b1a92e815310cd60888d928614157a584b15
Alejandro Medail <amedail.design@gmail.com>
2024-07-07 16:48:01 -0300

    Part 2 exercise 2.5: Course Information, step 10

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -1,33 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => (
-  <li>
-    {part.name} {part.exercises}
-  </li>
-)
-
-const Content = ({ parts }) => (
-  <ul>
-    {
-      parts.map(part => <Part key={part.id} part={part} />)
-    }   
-  </ul>
-) 
-
-const Course= ({ course }) => {
-
-  const totalExercises = course.parts.reduce((acc, item) => acc + item.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={totalExercises} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
