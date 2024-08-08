6cfc1f0b7833c1922ada1943f726407296aab238
Alejandro Medail <amedail.design@gmail.com>
2024-08-08 16:39:56 -0300

    Part 3 exercise 3.5: Phonebook backend step 5

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -48,6 +50,23 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  const person = {
+    id: Math.floor(Math.random() * 100000),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  console.log(persons)
+
+  response.json(person)
 })
 
 const PORT = 3001
